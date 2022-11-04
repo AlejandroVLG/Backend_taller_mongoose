c7955cfe1e978f772a8064a8f94e82e27cb0884f
Alexlg <Alejandrolg86@gmail.com>
2022-11-04 11:42:05 +0100

    index, config and app created

---
+++ index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const config = require('./config')
+const app = require('./app')
+
+mongoose.connect(config.db, (error, res) => {
+    if (error) {
+        return console.log(`Error al conectar a la base de datos: ${error}`)
+    } else {
+        console.log('Conexion a la base de datos establecida..')
+    }
+})
